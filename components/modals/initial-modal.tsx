93f94ef81e2b2ffc87988fd6c076361220ffc6bd
MayconDietrich <maycon.dietrich@gmail.com>
2023-09-09 10:01:05 -0300

    feat: upload server image

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -2,14 +2,14 @@
 
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import { FileUpload } from "@/components/file-upload";
 
 
 const formSchema = z.object({
@@ -42,7 +42,7 @@ const InitialModal = () => {
     console.log('values: ', values)
   }
 
-  if(!isMounted)
+  if (!isMounted)
     return null;
 
   return (
@@ -60,26 +60,40 @@ const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-ui-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO: Image upload
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
 
-              <FormField control={form.control} name="name" 
-              render={({field}) => (
-                <FormItem>
-                  <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
-                    Server Name
-                  </FormLabel>
-                  <FormControl>
-                    <Input disabled={isLoading} className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black" placeholder="Enter server name" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )} />
+              <FormField control={form.control} name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                      Server Name
+                    </FormLabel>
+                    <FormControl>
+                      <Input disabled={isLoading} className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black" placeholder="Enter server name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )} />
             </div>
             <DialogFooter className="bg-gray-100 px-6 py-4">
-                <Button disabled={isLoading} variant="primary">
-                  Create
-                </Button>
+              <Button disabled={isLoading} variant="primary">
+                Create
+              </Button>
             </DialogFooter>
           </form>
         </Form>
