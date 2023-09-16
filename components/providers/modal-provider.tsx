cf6e55b731396b458a3fea8f2a9275a0375e58b9
MayconDietrich <maycon.dietrich@gmail.com>
2023-09-16 15:06:23 -0300

    feat: edit server settings

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 import CreateServerModal from "@/components/modals/create-server-modal"
 import InviteModal from "@/components/modals/invite-modal";
+import EditServerModal from "@/components/modals/edit-server-modal";
 
 export const ModalProvider = () => {
   const [isMouted, setIsMouted] = useState(false);
@@ -17,8 +18,9 @@ export const ModalProvider = () => {
 
   return (
     <>
-      <CreateServerModal />;
+      <CreateServerModal />
       <InviteModal />
+      <EditServerModal />
     </>
   )
 }
