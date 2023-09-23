90b21f812c9547763183bfd92b7e1ebe829f30af
MayconDietrich <maycon.dietrich@gmail.com>
2023-09-23 13:28:34 -0300

    feat: redirect to selection

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -2,6 +2,7 @@
 
 import { Search } from "lucide-react";
 import { useEffect, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
 
 import {
   CommandDialog,
@@ -28,6 +29,9 @@ export const ServerSearch = ({
   data
 }: ServerSearchProps) => {
   const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const params = useParams();
+
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
@@ -39,6 +43,19 @@ export const ServerSearch = ({
     document.addEventListener("keydown", down);
     return () => document.removeEventListener("keydown", down);
   }, []);
+
+  const onClick = ({ id, type }: { id: string, type: "channel" | "member" }) => {
+    setOpen
+      (false);
+
+    if (type === "member") {
+      return router.push(`/servers/${params?.serverId}/conversations/${id}`)
+    }
+
+    if (type === "channel") {
+      return router.push(`/servers/${params?.serverId}/channels/${id}`)
+    }
+  }
 
 
   return (
@@ -71,7 +88,7 @@ export const ServerSearch = ({
               <CommandGroup key={label} heading={label}>
                 {data?.map(({ id, icon, name }) => {
                   return (
-                    <CommandItem key={id}>
+                    <CommandItem key={id} onSelect={() => onClick({ id, type })}>
                       {icon}
                       <span>{name}</span>
                     </CommandItem>
