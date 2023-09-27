741b1ee203af0e68c0db4ca1bab1112064c5a6ab
MayconDietrich <maycon.dietrich@gmail.com>
2023-09-27 18:53:01 -0300

    feat: delete channel modal

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -1,16 +1,17 @@
 "use client";
 
-import { 
-  Channel, 
-  ChannelType, 
-  MemberRole, 
-  Server 
+import {
+  Channel,
+  ChannelType,
+  MemberRole,
+  Server
 } from "@prisma/client";
 import { useParams, useRouter } from "next/navigation";
 import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { ActionTooltip } from "@/components/action-tooltip";
+import { useModal } from "@/hooks/use-modal-stores";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -25,10 +26,11 @@ const iconMap = {
 }
 
 export const ServerChannel = ({
-  channel, 
+  channel,
   server,
   role
 }: ServerChannelProps) => {
+  const { onOpen } = useModal();
   const params = useParams();
   const router = useRouter();
 
@@ -36,13 +38,13 @@ export const ServerChannel = ({
 
   return (
     <button
-      onClick={() => {}}
+      onClick={() => { }}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
       )}
     >
-      <Icon className="flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400"/>
+      <Icon className="flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400" />
       <p className={cn(
         "line-clamp-1 font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
         params?.channelId === channel.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
@@ -57,7 +59,9 @@ export const ServerChannel = ({
             " />
           </ActionTooltip>
           <ActionTooltip label="Delete">
-            <Trash className="
+            <Trash
+              onClick={() => { onOpen("deleteChannel", { server, channel }) }}
+              className="
               hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition
             " />
           </ActionTooltip>
