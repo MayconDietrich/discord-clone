cfb08ec69aa027b4b02d101fc80c1d397e79312b
MayconDietrich <maycon.dietrich@gmail.com>
2023-10-08 09:32:56 -0300

    feat: message file modal

--- hooks/use-modal-stores.ts
+++ hooks/use-modal-stores.ts
@@ -1,12 +1,14 @@
 import { Channel, ChannelType, Server } from "@prisma/client";
 import { create } from "zustand";
 
-export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel" | "editChannel";
+export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel" | "editChannel" | "messageFile";
 
 interface ModalData {
   server?: Server;
   channel?: Channel;
   channelType?: ChannelType;
+  apiUrl?: string;
+  query?: Record<string, any>;
 }
 
 interface ModalStore {
@@ -21,6 +23,6 @@ export const useModal = create<ModalStore>((set) => ({
   type: null,
   isOpen: false,
   data: {},
-  onOpen: (type, data = {}) => set({isOpen: true, type, data}),
-  onClose: () => set({type: null, isOpen: false})
+  onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
+  onClose: () => set({ type: null, isOpen: false })
 }));
