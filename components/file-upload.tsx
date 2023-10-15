62382137bf6da6376d8765971b0fc4741505086e
MayconDietrich <maycon.dietrich@gmail.com>
2023-10-15 11:04:51 -0300

    style: missing property

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -41,13 +41,13 @@ export const FileUpload = ({
 
   if (value && fileType === "pdf") {
     return (
-      <div className="relative flex items p-2 mt-2 rounded-md bg-background/10">
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
         <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
         <a
           href={value}
           target="_blank"
           rel="noopener noreferrer"
-          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:indeline"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
         >
           {value}
         </a>
