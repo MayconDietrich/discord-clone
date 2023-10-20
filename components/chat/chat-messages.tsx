33a987e4a07d9f07469439b57506eb4f872c9d8b
MayconDietrich <maycon.dietrich@gmail.com>
2023-10-20 20:14:56 -0300

    fix: node version error

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import { Fragment } from "react";
+import { format } from "date-fns";
 import { Member, Message, Profile } from "@prisma/client";
 import { Loader2, ServerCrash } from "lucide-react";
 
 import { ChatWelcome } from "./chat-welcome";
+import { ChatItem } from "./chat-item";
 
 import { useChatQuery } from "@/hooks/use-chat-query";
 
-type messageWithMemberWithProfile = Message & {
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
+
+type MessageWithMemberWithProfile = Message & {
   member: Member & {
-    Profile: Profile
+    profile: Profile
   }
 }
 
@@ -84,10 +88,20 @@ export const ChatMessages = ({
       <div className="flex flex-col-reverse mt-auto">
         {data?.pages?.map((group, i) => (
           <Fragment key={i}>
-            {group.items.map((message: messageWithMemberWithProfile) => (
-              <div key={message.id}>
-                {message.content}
-              </div>
+            {group.items.map((message: MessageWithMemberWithProfile) => (
+              <ChatItem
+                key={message.id}
+                id={message.id}
+                currentMember={member}
+                member={message.member}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />
             ))}
           </Fragment>
         ))}
